9992f990867c13d445ec4841b0c60b4383fa52e9
Henry Salliss <henry.salliss@digital.homeoffice.gov.uk>
2025-09-12 11:58:16 +0100

    modify port for azure

--- server/index.ts
+++ server/index.ts
@@ -59,7 +59,8 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
+// Use Azure's port if available, otherwise default to 5000
+const port = process.env.PORT ? parseInt(process.env.PORT) : 5000;
   server.listen(
     {
       port,
